919943f27f32188bc94d68348a93482294d1071b
Vivek S <vsbalussery@gmail.com>
2023-12-06 23:53:59 +0530

    Solution added to play the notes in order

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,39 @@
 import { notesToPlayInOrder } from "./music-to-play";
+import { MusicalNote } from "./musical-score";
 
-function playMusic() {
+async function playMusic() {
     const notes = notesToPlayInOrder;
-    
-    // TODO Play these notes one after the other at the pitch and rhythm given in each note
+
+    playAudio(0);
+
 }
 
+/**
+ * plays the audio from the musicalNote array
+ * @param index
+ */
+function playAudio(index: number) {
+    if (index < notesToPlayInOrder.length) {
+        const accidentals = {
+            "F": "Flat",
+            "S": "Sharp"
+        }
+        let audioFile = "./src/assets/" + notesToPlayInOrder[index].pitch + notesToPlayInOrder[index].octave;
+        if (notesToPlayInOrder[index].accidental != undefined) {
+            audioFile += " " + accidentals[notesToPlayInOrder[index].accidental] ;
+        }
+        audioFile +=  ".mp3"
+        console.log(index)
+      const audio = new Audio(audioFile);
+      audio.play();
+      setTimeout(() => {
+        audio.pause();
+        playAudio(index + 1);
+      }, notesToPlayInOrder[index].beats * 1000);
+    }
+  }
+
 document.getElementById('start-playing')?.addEventListener('click', playMusic);
+function sleep(arg0: number) {
+    throw new Error("Function not implemented.");
+}
